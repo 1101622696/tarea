6d8053116974b353269a282e5d284c88c9e2c098
Dcardenas <126295398+1101622696@users.noreply.github.com>
2024-03-05 12:16:03 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,8 @@
-<template>
-  <div class="principal">
-    <div id="alerta">
-    <textarea  v-if="alerta" v-model="alerta"> </textarea>
-<button @click="closealert()" v-show="alerta">cerrar</button>
-</div>
 
-    <div class="user_prin" id="user_prin" v-show="data_user">
-      <p style="font-weight: 900">Datos Registro </p>
-      <input class="controls" type="number" id="pre" placeholder="Ingresar saldo inicial" v-model="valor" />
-      <input class="controls" type="text" id="user" placeholder="Ingresar usuario" v-model="user" />
-      <input class="controls" type="text" id="contra" placeholder="Ingresar contraseña" v-model="contra" />
-      <button class="button2" @click="validar()">Agregar</button>
-    </div>
-
-    <div class="caja" id="cajaDiv" v-show="data_contened">
-      <div class="meter">
-        <h2>Tu saldo es ${{ valor }}</h2>
-        <h4>Usuario actual {{ user }}</h4>
-        <form @submit.prevent="realizarOperacion">
-          <select class="type_register" v-model="operacion">
-            <option value="consignar">Consignar</option>
-            <option value="retirar">Retirar</option>
-          </select>
-          <input class="controls" type="number" v-model="plus" placeholder="Ingrese un valor">
-          <button class="button1">Agregar</button>
-        </form>
-      </div>
-
-      <div class="retiros" v-show="data_retiros">
-        <h2>Nomenclatura del retiro</h2>
-        <table style="width: 100%;">
-          <tbody id="t">
-            <tr v-for="(cantidad, denominacion) in desgloseBilletes" :key="denominacion">
-              <td>Entregado {{ cantidad }} billete(s) de {{ denominacion }}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
-</template>
 
 <script setup>
+
+import Swal from 'sweetalert2';
 
 function closealert() {
   alerta.value=""
@@ -65,13 +26,13 @@ function cambiardivs() {
 }
 const validar = () => {
   if (user.value === "") {
-    alerta.value="Nombre de usuario no puede estar vacio"
+    mostrarError("Nombre de usuario no puede estar vacio");
   } else if (valor.value === "") {
-    alerta.value="Debe ingresar un valor"
+    mostrarError("Debe ingresar un valor");
   } else if (contra.value === "") {
-    alerta.value="Ingrese una contraseña"
+    mostrarError("Ingrese una contraseña");
   } else {
-    alerta.value="Registro de usuario exitoso"
+    mostrarmensaje("Registro de usuario exitoso");
     users.value.push({
       nombre: user.value,
       contrasena: contra.value,
@@ -94,7 +55,7 @@ const consignar = () => {
     alerta.value="Ingrese un valor válido para la consignación";
   } else {
     valor.value = Number(valor.value) + Number(plus.value);
-    alerta.value=`Consignación exitosa. Nuevo saldo: $${valor.value}`;
+    mostrarmensaje(`Consignación exitosa. Nuevo saldo: $${valor.value}`);
   }
 };
 
@@ -103,20 +64,20 @@ const pedirContrasena = () => {
   if (inputPassword === users.value.find(u => u.nombre === user.value)?.contrasena) {
     retirar();
   } else {
-    alerta.value="Contraseña incorrecta. No se puede realizar el retiro";
+    mostrarError("Contraseña incorrecta. No se puede realizar el retiro");
   }
 };
 
 const retirar = () => {
   if (isNaN(Number(plus.value)) || Number(plus.value) <= 0) {
-    alerta.value="Ingrese un valor válido para el retiro"
+    mostrarError("Ingrese un valor válido para el retiro");
   } else if (Number(plus.value) % 10000 !== 0) {
-    alerta.value="Solo se permiten retiros en múltiplos de 10000"
+    mostrarError("Solo se permiten retiros en múltiplos de 10000" );
   } else if (Number(plus.value) > Number(valor.value)) {
-    alerta.value="No tiene suficiente saldo para realizar el retiro";
+    mostrarError("No tiene suficiente saldo para realizar el retiro");
   } else {
     valor.value = Number(valor.value) - Number(plus.value);
-    alerta.value=`Retiro exitoso. Nuevo saldo: $${valor.value}`;
+    mostrarmensaje(`Retiro exitoso. Nuevo saldo: $${valor.value}`);
     actualizarDesgloseBilletes(Number(plus.value));
   }
 };
@@ -140,21 +101,116 @@ const actualizarDesgloseBilletes = (monto) => {
   }
 };
 
+function mostrarError(mensaje) {
+  Swal.fire({
+    icon: "error",
+    title: mensaje,
+    text: "Vuelve e intenta!",
+  });
+}
+function mostrarmensaje(mensaje) {
+  Swal.fire({
+    icon: "success",
+    title: mensaje,
+    text: "Bien hecho",
+  });
+}
+
 </script>
 
 
+
+<template>
+  <div class="principal">
+    <div class="user_prin" id="user_prin" v-show="data_user">
+      <p id="datos_t">Datos Registro </p>
+      <input class="controls" type="number" id="pre" placeholder="Ingresar saldo inicial" v-model="valor" />
+      <input class="controls" type="text" id="user" placeholder="Ingresar usuario" v-model="user" />
+      <input class="controls" type="text" id="contra" placeholder="Ingresar contraseña" v-model="contra" />
+      <button class="button2" @click="validar()">Agregar</button>
+    </div>
+
+    <div class="caja" id="cajaDiv" v-show="data_contened">
+      <div class="meter">
+        <h2 id="saldo_es">Tu saldo es ${{ valor }}</h2>
+        <h4>Usuario actual: {{ user }}</h4>
+        <form @submit.prevent="realizarOperacion">
+          <select class="type_register" v-model="operacion">
+            <option value="consignar">Consignar</option>
+            <hr>
+            <option value="retirar">Retirar</option>
+          </select>
+          <input class="controls" type="number" v-model="plus" placeholder="Ingrese un valor">
+          <button class="button1">Agregar</button>
+        </form>
+      </div>
+
+      <div class="retiros" v-show="data_retiros">
+        <h2>Nomenclatura del retiro</h2>
+        <table style="width: 100%;">
+          <tbody id="t">
+            <tr v-for="(cantidad, denominacion) in desgloseBilletes" :key="denominacion">
+              <td>Entregado {{ cantidad }} billete(s) de {{ denominacion }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
 <style coped>
-#alerta{
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  width: 50px;
-  height: 100px;
+.principal{
+  width:80vw;
+  height:80vh;
+  background-color: rgb(168, 168, 241);
+}
+
+.user_prin{
+  width:70%;
+  height:60%;
+  margin-left: auto;
+  margin-right: auto;
+  border:2px solid black
+}
+#datos_t{
+  color:black;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-size: x-large;
+  font-weight: 300;
+  letter-spacing: 2px;
+}
+
+.controls{
+  width:10vw;
+  height:5vh;
+  background-color: beige;
+  color:black
+}
+
+.button2{
+  background-color: rgb(82, 73, 73);
+  margin-left:10%
+}
+
+#saldo_es{
+color:black;
+letter-spacing: 1px;
+}
+h4{
+color:black;
+font-size: larger;
+}
+
+.type_register{
+  background-color: azure;
+  width:8vw;
+  height:4vh;
+  color:black;
 
 }
-textarea{
-  background:rgb(206, 65, 65);
-  padding: 4%;
-  font-size: large;
-}
+
+
 </style>
